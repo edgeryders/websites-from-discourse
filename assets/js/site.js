ef56b192c9a318291d00dfa5df396edac03ecd87
damingo <daniel@ansorgs.de>
2019-05-28 17:45:47 +0200

    Add  particle animation

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,8 +1,36 @@
 import $ from 'jquery';
 import 'bootstrap';
+import particlesJS from 'particlesjs';
 
-console.log('hello from webpack');
+// console.log('hello from webpack');
+//
+// $('#exampleModal').on('show.bs.modal', function (e) {
+//   console.log('opened');
+// });
 
-$('#exampleModal').on('show.bs.modal', function (e) {
-  console.log('opened');
-});
+
+window.onload = function () {
+    // https://github.com/marcbruederlin/particles.js
+    particlesJS.init({
+        selector: '#particles-js',
+        color: '#868686',
+        connectParticles: true,
+        sizeVariations: 2,
+        maxParticles: 70,
+        speed: 0.4,
+        responsive: [
+            {
+                breakpoint: 768,
+                options: {
+                    maxParticles: 30,
+                }
+            }, {
+                breakpoint: 425,
+                options: {
+                    maxParticles: 0,
+                }
+            }
+        ]
+
+    });
+};
--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -5,17 +5,22 @@ module ApplicationHelper
 
   # section:
   # locale:
-  def get_discourse_post(args={})
-
+  def get_discourse_post(args = {})
     api_key = ENV['DISCOURSE_API_KEY']
     id = data.discourse_sources.send(args[:section]).send(args[:locale])
 
     uri = URI.parse("https://edgeryders.eu/raw/#{id}?api_key=#{api_key}")
     response = Net::HTTP.get_response(uri)
-
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
-
     markdown.render(response.body.force_encoding("UTF-8"))
+  end
+
+
+  # See https://edgeryders.eu/c/ioh.json for the available attributes.
+  def get_most_recent_topics
+    uri = URI.parse("https://edgeryders.eu/c/ioh.json")
+    response = Net::HTTP.get_response(uri)
+    JSON.parse(response.body.force_encoding("UTF-8"))
   end
 
 
