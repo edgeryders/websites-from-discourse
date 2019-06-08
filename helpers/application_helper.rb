f29a5e253cc7de6b2deea58e62ef2cee39ae14d7
damingo <daniel@ansorgs.de>
2019-06-08 11:40:21 +0200

    Fixes #25

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -8,13 +8,14 @@ module ApplicationHelper
   def get_discourse_post(args = {})
     api_key = ENV['DISCOURSE_API_KEY']
     id = data.discourse_sources.send(args[:section]).send(args[:locale])
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
 
     uri = URI.parse("https://edgeryders.eu/raw/#{id}?api_key=#{api_key}")
     response = Net::HTTP.get_response(uri)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
-    markdown.render(response.body.force_encoding("UTF-8"))
+    # NOTE: gsub removes HTML comments. Otherwise they are rendered as plain text.
+    text = response.body.force_encoding("UTF-8").gsub(/<!--.*?-->/m, '')
+    markdown.render(text)
   end
-
 
   # See https://edgeryders.eu/c/ioh.json for the available attributes.
   def get_most_recent_topics
@@ -23,12 +24,26 @@ module ApplicationHelper
     JSON.parse(response.body.force_encoding("UTF-8"))
   end
 
+  def get_tell_your_story_topics
+    uri = URI.parse("https://edgeryders.eu/c/ioh/tell-your-story.json")
+    response = Net::HTTP.get_response(uri)
+    JSON.parse(response.body.force_encoding("UTF-8"))
+  end
 
   # See https://edgeryders.eu/tags/c/ioh/events/event.json for the available attributes.
   def get_event_topics
     uri = URI.parse("https://edgeryders.eu/c/ioh/events/l/agenda.json")
     response = Net::HTTP.get_response(uri)
     JSON.parse(response.body.force_encoding("UTF-8"))
+  end
+
+  # Source: https://github.com/middleman/middleman/issues/1490
+  def i18n_path(path, options = {})
+    lang = options[:language] ? options[:language] : I18n.locale.to_s
+    # English is mounted at root
+    lang = '' if lang == 'en'
+    # Replace multiple slashes with one
+    "/#{lang}/#{path}".gsub(/\/+/, '/')
   end
 
 
