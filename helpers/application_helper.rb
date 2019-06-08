e5f3318267d61db8486e60d6c5de86a09fd60500
damingo <daniel@ansorgs.de>
2019-06-08 16:04:08 +0200

    Fixes #31

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -19,20 +19,39 @@ module ApplicationHelper
 
   # See https://edgeryders.eu/c/ioh.json for the available attributes.
   def get_most_recent_topics
-    uri = URI.parse("https://edgeryders.eu/c/ioh.json")
-    response = Net::HTTP.get_response(uri)
-    JSON.parse(response.body.force_encoding("UTF-8"))
+    get_json_data('https://edgeryders.eu/c/ioh.json')
   end
 
   def get_tell_your_story_topics
-    uri = URI.parse("https://edgeryders.eu/c/ioh/tell-your-story.json")
-    response = Net::HTTP.get_response(uri)
-    JSON.parse(response.body.force_encoding("UTF-8"))
+    get_json_data('https://edgeryders.eu/c/ioh/tell-your-story.json')
   end
 
   # See https://edgeryders.eu/tags/c/ioh/events/event.json for the available attributes.
   def get_event_topics
-    uri = URI.parse("https://edgeryders.eu/c/ioh/events/l/agenda.json")
+    get_json_data('https://edgeryders.eu/c/ioh/events/l/agenda.json')
+  end
+
+  def get_tell_your_story_stats
+    page = 0
+    stats = OpenStruct.new(user_count: 0, topic_count: 0, comment_count: 0)
+    user_ids = []
+    loop do
+      results = get_json_data("https://edgeryders.eu/c/ioh/tell-your-story.json?page=#{page}")
+      if results['users'].present?
+        user_ids += results['users'].map {|u| u['id']}
+        stats.topic_count += results['topic_list']['topics'].count
+        stats.comment_count += results['topic_list']['topics'].sum {|t| t['posts_count'] -1 }
+        page += 1
+      else
+        break
+      end
+    end
+    stats.user_count = user_ids.uniq.count
+    stats
+  end
+
+  def get_json_data(url)
+    uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     JSON.parse(response.body.force_encoding("UTF-8"))
   end
