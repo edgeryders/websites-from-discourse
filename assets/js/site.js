647c86cf026c3999659c88cab3cd2658747a103b
tanius <matthias@ansorgs.de>
2019-09-15 16:40:54 +0200

    removing particle animation

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,5 @@
 import $ from 'jquery';
 import 'bootstrap';
-import particlesJS from 'particlesjs';
 
 // console.log('hello from webpack');
 //
@@ -17,29 +16,3 @@ $(function () {
 $('#storyboard-carousel').carousel({
     interval: false
 });
-
-window.onload = function () {
-    // https://github.com/marcbruederlin/particles.js
-    particlesJS.init({
-        selector: '#particles-js',
-        color: '#868686',
-        connectParticles: true,
-        sizeVariations: 1,
-        maxParticles: 80,
-        speed: 0.35,
-        responsive: [
-            {
-                breakpoint: 768,
-                options: {
-                    maxParticles: 35,
-                }
-            }, {
-                breakpoint: 425,
-                options: {
-                    maxParticles: 0,
-                }
-            }
-        ]
-
-    });
-};
