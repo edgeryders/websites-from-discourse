8bcce9065864a8b4d0d6e2b01a68bb9d07a1a3bb
tanius <matthias@ansorgs.de>
2019-09-27 22:12:38 +0200

    support for Markdown inside HTML

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -3,12 +3,65 @@ require 'json'
 
 module ApplicationHelper
 
+  # Markdown renderer with support for Markdown inside HTML block-level elements.
+  #
+  # Usage: Since Redcarpet does not provide a way to access the renderer from inside Redcarpet::Markdown, as a
+  # workaround, we call Redcarpet::Markdown:render() from inside this renderer to enable recursive rendering of Markdown
+  # inside HTML. A reference to this method has to be supplied after calling the constructor, like this:
+  #
+  # renderer = RecursiveRenderer.new()
+  # markdown = Redcarpet::Markdown.new(renderer, autolink: true)
+  # renderer.set_render &markdown.method(:render)
+  #
+  # Source: Comment by @pienkowskip at Redcarpet issue #13, see:
+  #   https://github.com/vmg/redcarpet/issues/13#issuecomment-43154826
+  # Licence: As a contribution to Redcarpet, it is covered by the MIT Licence. See:
+  #   See: https://github.com/vmg/redcarpet/blob/master/COPYING
+  class RecursiveRenderer < Redcarpet::Render::HTML
+
+    # Override block_html to support parsing nested markdown blocks.
+    #
+    # @param [String] raw
+    def block_html(raw_html)
+      if (md = raw_html.match(/\<(.+?)\>(.*)\<(\/.+?)\>/m))
+        open_tag, content, close_tag = md.captures
+        "\n<#{open_tag}>\n#{render content}<#{close_tag}>\n"
+      else
+        raw_html
+      end
+    end
+
+    # Hack to allow using the Markdown renderer recursively by passing in a reference to Redcarpet::Markdown:render()
+    #
+    # @param [String] markdown
+    # @return [String]
+    def set_render(&block)
+      @render = block
+    end
+
+    # Recursively render mixed HTML/Markdown with this renderer.
+    #
+    # Assumes that @renderer has been properly set to Redcarpet::Markdown:render().
+    private
+    def render(markdown)
+      if @render.nil?
+        markdown
+      else
+        @render.call(markdown)
+      end
+    end
+  end
+
   # section:
   # locale:
   def get_discourse_post(args = {})
     api_key = ENV['DISCOURSE_API_KEY']
     id = data.discourse_sources.send(args[:section])
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+
+    # Use our custom renderer instead of the default Redcarpet::Render::HTML
+    renderer = RecursiveRenderer.new()
+    markdown = Redcarpet::Markdown.new(renderer, autolink: true)
+    renderer.set_render &markdown.method(:render)
 
     uri = URI.parse("https://edgeryders.eu/raw/#{id}?api_key=#{api_key}")
     response = Net::HTTP.get_response(uri)
